a9afd0e2cb4edf11006cae39c2025542358604bd
Maximilian König <47715517+makoeta@users.noreply.github.com>
2023-06-02 19:45:57 +0200

    Update bugapoint.sql
    ValidTill -> Validtill
    
    muss klein sonst wird es nicht richtig erkannt

--- bugapoint.sql
+++ bugapoint.sql
@@ -7,7 +7,7 @@ CREATE TABLE bugapoint (ID int(10) NOT NULL AUTO_INCREMENT, ParkID int(10) NOT N
 CREATE TABLE report (ID int(10) NOT NULL AUTO_INCREMENT, BugaPointID int(10) NOT NULL, AdminID int(10) NOT NULL, ReportText varchar(4095), PRIMARY KEY (ID)) ENGINE=InnoDB;
 CREATE TABLE admin (ID int(10) NOT NULL AUTO_INCREMENT, Firstname varchar(255), Lastname varchar(255), Emailadress varchar(255) UNIQUE, Password varchar(255), Role varchar(255), PRIMARY KEY (ID)) ENGINE=InnoDB;
 CREATE TABLE park (ID int(10) NOT NULL AUTO_INCREMENT, Title varchar(255), Latitude double NOT NULL, Longitude double NOT NULL, PRIMARY KEY (ID)) ENGINE=InnoDB;
-CREATE TABLE persondensityreport (ID int(10) NOT NULL AUTO_INCREMENT, Latitude double NOT NULL, Longitude double NOT NULL, ValidTill date, PRIMARY KEY (ID)) ENGINE=InnoDB;
+CREATE TABLE persondensityreport (ID int(10) NOT NULL AUTO_INCREMENT, Latitude double NOT NULL, Longitude double NOT NULL, Validtill date, PRIMARY KEY (ID)) ENGINE=InnoDB;
 CREATE TABLE route (ID int(10) NOT NULL AUTO_INCREMENT, Departure timestamp NULL, Arrival timestamp NULL, TransportationType int(10), PRIMARY KEY (ID)) ENGINE=InnoDB;
 ALTER TABLE bugapoint ADD CONSTRAINT manages FOREIGN KEY (AdminID) REFERENCES admin (ID);
 ALTER TABLE report ADD CONSTRAINT `works on` FOREIGN KEY (AdminID) REFERENCES admin (ID);
